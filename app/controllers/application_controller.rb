cb179ec782ce83af4bc136074f13656867f8a80a
laptop <arvind.123.dagar@gmail.com>
2014-12-15 12:04:03 +0530

    ui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
  def after_sign_in_path_for(resource)
-    if resource.admin? 
+    if resource.admin?
     	renewal_date
-       daily_updates_path
+      daily_updates_path
     else
-       new_daily_update_path
+      renewal_date
+      new_daily_update_path
     end
  end
-def renewal_date
-	
-	@contract=AddContract.where('renewal_date < ?', Date.today).update_all(:status=>"expired")
-  
-end
+  def renewal_date
+	  @contract=AddContract.where('renewal_date < ?', Date.today).update_all(:status=>"expired")
+  end
 end
 
