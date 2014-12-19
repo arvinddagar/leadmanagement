22d3a1e77c79d6e7d559327e38a041ad7f61f10a
laptop <arvind.123.dagar@gmail.com>
2014-12-19 17:18:35 +0530

    ui

--- app/models/add_contract.rb
+++ app/models/add_contract.rb
@@ -1,5 +1,5 @@
 class AddContract < ActiveRecord::Base
 	PLANS = ["1 month", "2 month","quaterly","half yearly","yearly"]
 	STATUS=["active","expired"]
-	WORKSTATUS=["Work not started","work started","ready for deployment","handed over to client","Working on revision","freezed"]
+	WORKSTATUS=["Work not started","Work started","Ready for deployment","Handed over to client","Working on revision","Freezed"]
 end
