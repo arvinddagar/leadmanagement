c98801c42f78e1af5e8c35864aa721cb53026ef5
laptop <arvind.123.dagar@gmail.com>
2014-12-24 11:35:18 +0530

    error

--- app/assets/javascripts/status_modal.js
+++ app/assets/javascripts/status_modal.js
@@ -1,16 +1,3 @@
  $(document).ready(function(){
-   $(".announce").click(function(){
-
-     $('#s_id').val($(this).attr('id'));
-      $.ajax('/fetch_daily',{
-      type: 'get',
-        dataType: 'html',
-        data: {daily_up: $(this).attr('id')},
-        success: function(data, textStatus, jqXHR)
-          {
-           $('#daily_update_id').val(data);
-          }
-    });
-
-   });
+  
 });
